842fbb1b70c8de3b53221a5d7e9b72b1a1ce2095
Darko <daki1305@gmail.com>
2018-07-13 01:32:02 +0200

    creating animation

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+var home = document.querySelector("#home");
+
+home.onclick = function(){
+    console.log("daada");
+    
+    setSectionToTheTop();
+}
+var about = document.querySelector("#about");
+
+about.onclick = function(){
+    setSectionToTheTop();
+}
+
+function setSectionToTheTop(){
+    console.log("da");
+}
